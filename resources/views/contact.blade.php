298afa0c17b02ebbbf9be74fbd03f75a8f7597f4
josephekpele <josephekpele99@gmail.com>
2021-04-06 05:51:25 +0000

    first commit

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,142 @@
+<!DOCTYPE html>
+<html style="font-size: 16px;">
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <meta name="keywords" content="">
+    <meta name="description" content="">
+    <meta name="page_type" content="np-template-header-footer-from-plugin">
+    <title>Contact</title>
+    <link rel="stylesheet" href={{asset('css/nicepage.css')}} media="screen">
+<link rel="stylesheet" href={{asset('css/Contact.css')}} media="screen">
+    <script class="u-script" type="text/javascript" src="{{asset('js/jquery-1.9.1.min.js')}}" defer=""></script>
+    <script class="u-script" type="text/javascript" src="{{asset('js/nicepage.js')}}" defer=""></script>
+    <meta name="generator" content="Nicepage 3.9.2, nicepage.com">
+    <link id="u-theme-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Playfair+Display:400,400i,500,500i,600,600i,700,700i,800,800i,900,900i|Raleway:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i">
+    
+    
+    <script type="application/ld+json">{
+		"@context": "http://schema.org",
+		"@type": "Organization",
+		"name": "",
+		"url": "index.html"
+}</script>
+    <meta property="og:title" content="Contact">
+    <meta property="og:type" content="website">
+    <meta name="theme-color" content="#ff7070">
+    <link rel="canonical" href="index.html">
+    <meta property="og:url" content="index.html">
+  </head>
+  <body class="u-body"><header class="u-clearfix u-header" id="sec-11b4"><div class="u-align-left u-clearfix u-sheet u-sheet-1">
+        <nav class="u-menu u-menu-dropdown u-offcanvas u-menu-1">
+          <div class="menu-collapse">
+            <a class="u-button-style u-nav-link" href="#">
+              <svg><use xlink:href="#menu-hamburger"></use></svg>
+              <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><defs><symbol id="menu-hamburger" viewBox="0 0 16 16" style="width: 16px; height: 16px;"><rect y="1" width="16" height="2"></rect><rect y="7" width="16" height="2"></rect><rect y="13" width="16" height="2"></rect>
+</symbol>
+</defs></svg>
+            </a>
+          </div>
+          <div class="u-custom-menu u-nav-container">
+            <ul class="u-nav u-unstyled"><li class="u-nav-item"><a class="u-button-style u-nav-link" href="/">Acceuil</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link" href="/reservation">Reservation</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link" href="/contact">Contact</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link" href="/connexion">Connexion</a>
+</li></ul>
+          </div>
+          <div class="u-custom-menu u-nav-container-collapse">
+            <div class="u-black u-container-style u-inner-container-layout u-opacity u-opacity-95 u-sidenav">
+              <div class="u-sidenav-overflow">
+                <div class="u-menu-close"></div>
+                <ul class="u-align-center u-nav u-popupmenu-items u-unstyled u-nav-2"><li class="u-nav-item"><a class="u-button-style u-nav-link" href="Acceuil.html">Acceuil</a>
+                </li><li class="u-nav-item"><a class="u-button-style u-nav-link" href="/reservation">Reservation</a>
+                </li><li class="u-nav-item"><a class="u-button-style u-nav-link" href="/contact">Contact</a>
+                </li><li class="u-nav-item"><a class="u-button-style u-nav-link" href="/connexion">Connexion</a>
+                </li></ul>
+              </div>
+            </div>
+            <div class="u-black u-menu-overlay u-opacity u-opacity-70"></div>
+          </div>
+        </nav>
+      </div></header>
+    <section class="u-clearfix u-grey-5 u-section-1" id="sec-a6d6">
+      <div class="u-clearfix u-sheet u-sheet-1">
+        <div class="u-form u-form-1">
+          <form action="https://nicepage.com/editor/Forms/Process" method="POST" class="u-clearfix u-form-spacing-10 u-form-vertical u-inner-form" style="padding: 10px" source="email" name="form" abineguid="67CFAFF2C10649B798AC71853E5CE270">
+            <input type="hidden" id="siteId" name="siteId" value="284254">
+            <input type="hidden" id="pageId" name="pageId" value="284257">
+            <div class="u-form-group u-form-name">
+              <label for="name-5572" class="u-form-control-hidden u-label">Nom</label>
+              <input type="text" placeholder="" id="name-5572" name="name" class="u-border-1 u-border-grey-30 u-input u-input-rectangle u-white" required="">
+            </div>
+            <div class="u-form-email u-form-group">
+              <label for="email-5572" class="u-form-control-hidden u-label">Email</label>
+              <input type="email" placeholder="Entrez une adresse mail valide" id="email-5572" name="email" class="u-border-1 u-border-grey-30 u-input u-input-rectangle u-white" required="">
+              <div id="pwm-inline-icon-74758" class="pwm-field-icon" style="margin-top: -32px; margin-left: 528px; position: absolute !important; width: 18px; height: 18px; min-height: 0px; min-width: 0px; z-index: 2147483645 !important; box-shadow: none !important; box-sizing: content-box !important; background: none !important; border: none !important; padding: 0px; cursor: pointer !important; outline: none !important;">
+                <svg style="display: inline-block !important; width: 16px; height: 16px; fill: rgb(230, 0, 23) !important; margin-top: 1px; position: absolute !important; top: 0px; left: 0px;" viewBox="0 0 40 64"><g><path d="m20,28.12a33.78,33.78 0 0 1 13.36,2.74a22.18,22.18 0 0 1 0.64,5.32c0,9.43 -5.66,17.81 -14,20.94c-8.34,-3.13 -14,-11.51 -14,-20.94a22.2,22.2 0 0 1 0.64,-5.32a33.78,33.78 0 0 1 13.36,-2.74m0,-28.12c-8.82,0 -14,7.36 -14,16.41l0,5.16c2,-1.2 2,-1.49 5,-2.08l0,-3.08c0,-6.21 2.9,-11.41 8.81,-11.41l0.19,0c6.6,0 9,4.77 9,11.41l0,3.08c3,0.58 3,0.88 5,2.08l0,-5.16c0,-9 -5.18,-16.41 -14,-16.41l0,0zm0,22c-6.39,0 -12.77,0.67 -18.47,4a31.6,31.6 0 0 0 -1.53,9.74c0,13.64 8.52,25 20,28.26c11.48,-3.27 20,-14.63 20,-28.26a31.66,31.66 0 0 0 -1.54,-9.77c-5.69,-3.3 -12.08,-4 -18.47,-4l0,0l0.01,0.03z"></path><path d="m21.23,39.5a2.81,2.81 0 0 0 1.77,-2.59a2.94,2.94 0 0 0 -3,-2.93a3,3 0 0 0 -3,3a2.66,2.66 0 0 0 1.77,2.48l-1.77,4.54l6,0l-1.77,-4.5z"></path>
+</g></svg>
+              </div>
+            </div>
+            <div class="u-form-group u-form-message">
+              <label for="message-5572" class="u-form-control-hidden u-label">Message</label>
+              <textarea placeholder="Entrez votre message" rows="4" cols="50" id="message-5572" name="message" class="u-border-1 u-border-grey-30 u-input u-input-rectangle u-white" required=""></textarea>
+            </div>
+            <div class="u-align-left u-form-group u-form-submit">
+              <a href="#" class="u-btn u-btn-submit u-button-style">Soumettre</a>
+              <input type="submit" value="submit" class="u-form-control-hidden">
+            </div>
+            <div class="u-form-send-message u-form-send-success"> Je vous remercie! Votre message a été envoyé. </div>
+            <div class="u-form-send-error u-form-send-message"> Impossible d'envoyer votre message. Veuillez corriger les erreurs puis réessayer. </div>
+            <input type="hidden" value="" name="recaptchaResponse">
+          </form>
+        </div>
+        <div class="u-grey-light-2 u-map u-map-1">
+          <div class="embed-responsive">
+            <iframe class="embed-responsive-item" src="//maps.google.com/maps?output=embed&amp;q=Manhattan%2C%20New%20York&amp;z=10&amp;t=m" data-map="JTdCJTIyYWRkcmVzcyUyMiUzQSUyMk1hbmhhdHRhbiUyQyUyME5ldyUyMFlvcmslMjIlMkMlMjJ6b29tJTIyJTNBMTAlMkMlMjJ0eXBlSWQlMjIlM0ElMjJyb2FkJTIyJTJDJTIybGFuZyUyMiUzQSUyMiUyMiU3RA=="></iframe>
+          </div>
+        </div>
+      </div>
+    </section>
+    
+    
+    <footer class="u-black u-clearfix u-footer" id="sec-e04b"><div class="u-clearfix u-sheet u-sheet-1">
+        <div class="u-clearfix u-expanded-width-lg u-expanded-width-md u-expanded-width-xl u-expanded-width-xs u-gutter-30 u-layout-wrap u-layout-wrap-1">
+          <div class="u-gutter-0 u-layout">
+            <div class="u-layout-row">
+              <div class="u-align-left u-container-style u-layout-cell u-left-cell u-size-20 u-size-20-md u-layout-cell-1">
+                <div class="u-container-layout u-valign-top u-container-layout-1">
+                  <h6 class="u-text u-text-1">Lieu</h6>
+                  <p class="u-small-text u-text u-text-variant u-text-2">Nukafu en face de la pharmacie Providence</p>
+                </div>
+              </div>
+              <div class="u-align-left u-container-style u-layout-cell u-size-20 u-size-20-md u-layout-cell-2">
+                <div class="u-container-layout u-valign-top u-container-layout-2">
+                  <h6 class="u-text u-text-3">Contacts</h6>
+                  <p class="u-small-text u-text u-text-variant u-text-4">00228 92134330 / 99672847</p>
+                </div>
+              </div>
+              <div class="u-align-left u-container-style u-layout-cell u-right-cell u-size-20 u-size-20-md u-layout-cell-3">
+                <div class="u-container-layout u-container-layout-3">
+                  <h6 class="u-text u-text-5">Liens</h6>
+                  <p class="u-small-text u-text u-text-variant u-text-6"><span class="u-icon u-icon-1"><svg class="u-svg-content" style="width: 1em; height: 1em;"><g><circle style="fill:currentColor;" cx="56.098" cy="56.098" r="56.098"></circle><path style="fill:#FFFFFF;" d="M70.201,58.294h-10.01v36.672H45.025V58.294h-7.213V45.406h7.213v-8.34   c0-5.964,2.833-15.303,15.301-15.303L71.56,21.81v12.51h-8.151c-1.337,0-3.217,0.668-3.217,3.513v7.585h11.334L70.201,58.294z"></path>
+</g></svg><img></span>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div></footer>
+    <section class="u-backlink u-clearfix u-grey-80">
+      <a class="u-link" href="https://nicepage.com/website-templates" target="_blank">
+        <span>Website Templates</span>
+      </a>
+      <p class="u-text">
+        <span>created with</span>
+      </p>
+      <a class="u-link" href="https://nicepage.com/" target="_blank">
+        <span>Website Builder Software</span>
+      </a>. 
+    </section>
+  </body>
+</html>
---
+++ resources/views/reservation.blade.php
@@ -0,0 +1,155 @@
+<!DOCTYPE html>
+<html style="font-size: 16px;">
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <meta name="keywords" content="">
+    <meta name="description" content="">
+    <meta name="page_type" content="np-template-header-footer-from-plugin">
+    <title>Reservation</title>
+    <link rel="stylesheet" href={{asset('css/nicepage.css')}} media="screen">
+<link rel="stylesheet" href={{asset('css/Reservation.css')}} media="screen">
+    <script class="u-script" type="text/javascript" src="{{asset('js/jquery-1.9.1.min.js')}}" defer=""></script>
+    <script class="u-script" type="text/javascript" src="{{asset('js/nicepage.js')}}" defer=""></script>
+    <meta name="generator" content="Nicepage 3.9.2, nicepage.com">
+    <link id="u-theme-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Playfair+Display:400,400i,500,500i,600,600i,700,700i,800,800i,900,900i|Raleway:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i">
+    
+    
+    <script type="application/ld+json">{
+		"@context": "http://schema.org",
+		"@type": "Organization",
+		"name": "",
+		"url": "index.html"
+}</script>
+    <meta property="og:title" content="Reservation">
+    <meta property="og:type" content="website">
+    <meta name="theme-color" content="#ff7070">
+    <link rel="canonical" href="index.html">
+    <meta property="og:url" content="index.html">
+  </head>
+  <body class="u-body"><header class="u-clearfix u-header" id="sec-11b4"><div class="u-align-left u-clearfix u-sheet u-sheet-1">
+        <nav class="u-menu u-menu-dropdown u-offcanvas u-menu-1">
+          <div class="menu-collapse">
+            <a class="u-button-style u-nav-link" href="#">
+              <svg><use xlink:href="#menu-hamburger"></use></svg>
+              <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><defs><symbol id="menu-hamburger" viewBox="0 0 16 16" style="width: 16px; height: 16px;"><rect y="1" width="16" height="2"></rect><rect y="7" width="16" height="2"></rect><rect y="13" width="16" height="2"></rect>
+</symbol>
+</defs></svg>
+            </a>
+          </div>
+          <div class="u-custom-menu u-nav-container">
+            <ul class="u-nav u-unstyled"><li class="u-nav-item"><a class="u-button-style u-nav-link" href="/">Acceuil</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link" href="/reservation">Reservation</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link" href="/contact">Contact</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link" href="/connexion">Connexion</a>
+</li></ul>
+          </div>
+          <div class="u-custom-menu u-nav-container-collapse">
+            <div class="u-black u-container-style u-inner-container-layout u-opacity u-opacity-95 u-sidenav">
+              <div class="u-sidenav-overflow">
+                <div class="u-menu-close"></div>
+                <ul class="u-align-center u-nav u-popupmenu-items u-unstyled u-nav-2"><li class="u-nav-item"><a class="u-button-style u-nav-link" href="Acceuil.html">Acceuil</a>
+                </li><li class="u-nav-item"><a class="u-button-style u-nav-link" href="/reservation">Reservation</a>
+                </li><li class="u-nav-item"><a class="u-button-style u-nav-link" href="/contact">Contact</a>
+                </li><li class="u-nav-item"><a class="u-button-style u-nav-link" href="/connexion">Connexion</a>
+                </li></ul>
+              </div>
+            </div>
+            <div class="u-black u-menu-overlay u-opacity u-opacity-70"></div>
+          </div>
+        </nav>
+      </div></header>
+    <section class="u-clearfix u-palette-1-base u-section-1" id="carousel_5a81">
+      <div class="u-clearfix u-sheet u-valign-middle u-sheet-1">
+        <div class="u-clearfix u-expanded-width u-gutter-0 u-layout-wrap u-layout-wrap-1">
+          <div class="u-layout">
+            <div class="u-layout-row">
+              <div class="u-container-style u-layout-cell u-left-cell u-size-30 u-layout-cell-1">
+                <div class="u-container-layout u-valign-middle u-container-layout-1">
+                  <h5 class="u-text u-text-1">Best In City</h5>
+                  <h3 class="u-text u-text-2">To book Taxi Please use the quick reservation form</h3>
+                  <p class="u-text u-text-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam sed aliquet justo. Quisque ut mollis est. Morbi sed laoreet enim. Donec eget tortor nulla. Ut dolor leo, convallis eget ipsum et, rutrum ornare sem.&nbsp;</p>
+                  <a href="https://www.nicepage.com/website-builder" class="u-btn u-button-style u-btn-1">Button</a>
+                </div>
+              </div>
+              <div class="u-align-left u-black u-container-style u-layout-cell u-right-cell u-size-30 u-layout-cell-2">
+                <div class="u-container-layout u-valign-middle">
+                  <div class="u-expanded-width u-form u-form-1">
+                    <form action="https://nicepage.com/editor/Forms/Process" method="POST" class="u-clearfix u-form-spacing-10 u-form-vertical u-inner-form" style="padding: 40px;" source="email" name="form" abineguid="28F63124217B4F0DBF0E0968DF0B9115">
+                      <input type="hidden" id="siteId" name="siteId" value="284254">
+                      <input type="hidden" id="pageId" name="pageId" value="284256">
+                      <div class="u-form-group u-form-name">
+                        <label for="name-a3f0" class="u-form-control-hidden u-label">Name</label>
+                        <input type="text" placeholder="Enter your Name" id="name-a3f0" name="name" class="u-border-1 u-border-grey-30 u-input u-input-rectangle u-white" required="">
+                      </div>
+                      <div class="u-form-email u-form-group">
+                        <label for="email-a3f0" class="u-form-control-hidden u-label">Email</label>
+                        <input type="email" placeholder="Enter a valid email address" id="email-a3f0" name="email" class="u-border-1 u-border-grey-30 u-input u-input-rectangle u-white" required="">
+                        <div id="pwm-inline-icon-75265" class="pwm-field-icon" style="margin-top: -32px; margin-left: 468px; position: absolute !important; width: 18px; height: 18px; min-height: 0px; min-width: 0px; z-index: 2147483645 !important; box-shadow: none !important; box-sizing: content-box !important; background: none !important; border: none !important; padding: 0px; cursor: pointer !important; outline: none !important;">
+                          <svg style="display: inline-block !important; width: 16px; height: 16px; fill: rgb(230, 0, 23) !important; margin-top: 1px; position: absolute !important; top: 0px; left: 0px;" viewBox="0 0 40 64"><g><path d="m20,28.12a33.78,33.78 0 0 1 13.36,2.74a22.18,22.18 0 0 1 0.64,5.32c0,9.43 -5.66,17.81 -14,20.94c-8.34,-3.13 -14,-11.51 -14,-20.94a22.2,22.2 0 0 1 0.64,-5.32a33.78,33.78 0 0 1 13.36,-2.74m0,-28.12c-8.82,0 -14,7.36 -14,16.41l0,5.16c2,-1.2 2,-1.49 5,-2.08l0,-3.08c0,-6.21 2.9,-11.41 8.81,-11.41l0.19,0c6.6,0 9,4.77 9,11.41l0,3.08c3,0.58 3,0.88 5,2.08l0,-5.16c0,-9 -5.18,-16.41 -14,-16.41l0,0zm0,22c-6.39,0 -12.77,0.67 -18.47,4a31.6,31.6 0 0 0 -1.53,9.74c0,13.64 8.52,25 20,28.26c11.48,-3.27 20,-14.63 20,-28.26a31.66,31.66 0 0 0 -1.54,-9.77c-5.69,-3.3 -12.08,-4 -18.47,-4l0,0l0.01,0.03z"></path><path d="m21.23,39.5a2.81,2.81 0 0 0 1.77,-2.59a2.94,2.94 0 0 0 -3,-2.93a3,3 0 0 0 -3,3a2.66,2.66 0 0 0 1.77,2.48l-1.77,4.54l6,0l-1.77,-4.5z"></path>
+</g></svg>
+                        </div>
+                      </div>
+                      <div class="u-form-group u-form-message">
+                        <label for="message-a3f0" class="u-form-control-hidden u-label">Message</label>
+                        <textarea placeholder="Enter your message" rows="4" cols="50" id="message-a3f0" name="message" class="u-border-1 u-border-grey-30 u-input u-input-rectangle u-white" required=""></textarea>
+                      </div>
+                      <div class="u-form-group u-form-submit">
+                        <a href="#" class="u-btn u-btn-submit u-button-style">Submit</a>
+                        <input type="submit" value="submit" class="u-form-control-hidden">
+                      </div>
+                      <div class="u-form-send-message u-form-send-success"> Thank you! Your message has been sent. </div>
+                      <div class="u-form-send-error u-form-send-message"> Unable to send your message. Please fix errors then try again. </div>
+                      <input type="hidden" value="" name="recaptchaResponse">
+                    </form>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    
+    
+    <footer class="u-black u-clearfix u-footer" id="sec-e04b"><div class="u-clearfix u-sheet u-sheet-1">
+        <div class="u-clearfix u-expanded-width-lg u-expanded-width-md u-expanded-width-xl u-expanded-width-xs u-gutter-30 u-layout-wrap u-layout-wrap-1">
+          <div class="u-gutter-0 u-layout">
+            <div class="u-layout-row">
+              <div class="u-align-left u-container-style u-layout-cell u-left-cell u-size-20 u-size-20-md u-layout-cell-1">
+                <div class="u-container-layout u-valign-top u-container-layout-1">
+                  <h6 class="u-text u-text-1">Lieu</h6>
+                  <p class="u-small-text u-text u-text-variant u-text-2">Nukafu en face de la pharmacie Providence</p>
+                </div>
+              </div>
+              <div class="u-align-left u-container-style u-layout-cell u-size-20 u-size-20-md u-layout-cell-2">
+                <div class="u-container-layout u-valign-top u-container-layout-2">
+                  <h6 class="u-text u-text-3">Contacts</h6>
+                  <p class="u-small-text u-text u-text-variant u-text-4">00228 92134330 / 99672847</p>
+                </div>
+              </div>
+              <div class="u-align-left u-container-style u-layout-cell u-right-cell u-size-20 u-size-20-md u-layout-cell-3">
+                <div class="u-container-layout u-container-layout-3">
+                  <h6 class="u-text u-text-5">Liens</h6>
+                  <p class="u-small-text u-text u-text-variant u-text-6"><span class="u-icon u-icon-1"><svg class="u-svg-content" style="width: 1em; height: 1em;"><g><circle style="fill:currentColor;" cx="56.098" cy="56.098" r="56.098"></circle><path style="fill:#FFFFFF;" d="M70.201,58.294h-10.01v36.672H45.025V58.294h-7.213V45.406h7.213v-8.34   c0-5.964,2.833-15.303,15.301-15.303L71.56,21.81v12.51h-8.151c-1.337,0-3.217,0.668-3.217,3.513v7.585h11.334L70.201,58.294z"></path>
+</g></svg><img></span>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div></footer>
+    <section class="u-backlink u-clearfix u-grey-80">
+      <a class="u-link" href="https://nicepage.com/website-templates" target="_blank">
+        <span>Website Templates</span>
+      </a>
+      <p class="u-text">
+        <span>created with</span>
+      </p>
+      <a class="u-link" href="https://nicepage.com/" target="_blank">
+        <span>Website Builder Software</span>
+      </a>. 
+    </section>
+  </body>
+</html>
